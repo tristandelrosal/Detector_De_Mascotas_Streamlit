0f5cf88ca603907eefb35e6b8a7b307ae9e97ce9
Tristan Del Rosal <tristandelrosal@gmail.com>
2024-12-11 12:15:53 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Clasificador de Mascotas
+
+Aplicación hecha en Stream para clase cargando un modelo previamente entrenado que clasifica mascotas en funcion del peso, altura, color de ojos y longitud del pelo.
---
+++ streamlit_app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("Classipet")
+st.write("Clasificador de mascotas. Introzca los datos de su mascota y le diremos a que clase pertenece.")
+
+st.write("Por favor, introduzca los datos de su mascota:")
+
+
